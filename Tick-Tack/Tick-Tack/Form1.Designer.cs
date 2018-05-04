8fe435841c9af07fbd0eb916d3b22006ca7318f0
yasin-mehdiyev <30857286+yasin-mehdiyev@users.noreply.github.com>
2018-05-04 08:59:39 +0400

    Add files via upload

---
+++ Tick-Tack/Tick-Tack/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Tick_Tack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Btn6 = new System.Windows.Forms.Button();
+            this.Btn5 = new System.Windows.Forms.Button();
+            this.Btn4 = new System.Windows.Forms.Button();
+            this.Btn9 = new System.Windows.Forms.Button();
+            this.Btn8 = new System.Windows.Forms.Button();
+            this.Btn7 = new System.Windows.Forms.Button();
+            this.LblSira = new System.Windows.Forms.Label();
+            this.lblNovbe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblxscore = new System.Windows.Forms.Label();
+            this.lbloscore = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(233, 81);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(73, 44);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Location = new System.Drawing.Point(312, 81);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(73, 44);
+            this.Btn2.TabIndex = 0;
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Location = new System.Drawing.Point(391, 81);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(73, 44);
+            this.Btn3.TabIndex = 0;
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn6
+            // 
+            this.Btn6.Location = new System.Drawing.Point(391, 131);
+            this.Btn6.Name = "Btn6";
+            this.Btn6.Size = new System.Drawing.Size(73, 44);
+            this.Btn6.TabIndex = 1;
+            this.Btn6.UseVisualStyleBackColor = true;
+            this.Btn6.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn5
+            // 
+            this.Btn5.Location = new System.Drawing.Point(312, 131);
+            this.Btn5.Name = "Btn5";
+            this.Btn5.Size = new System.Drawing.Size(73, 44);
+            this.Btn5.TabIndex = 2;
+            this.Btn5.UseVisualStyleBackColor = true;
+            this.Btn5.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn4
+            // 
+            this.Btn4.Location = new System.Drawing.Point(233, 131);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.Size = new System.Drawing.Size(73, 44);
+            this.Btn4.TabIndex = 3;
+            this.Btn4.UseVisualStyleBackColor = true;
+            this.Btn4.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn9
+            // 
+            this.Btn9.Location = new System.Drawing.Point(391, 181);
+            this.Btn9.Name = "Btn9";
+            this.Btn9.Size = new System.Drawing.Size(73, 44);
+            this.Btn9.TabIndex = 4;
+            this.Btn9.UseVisualStyleBackColor = true;
+            this.Btn9.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn8
+            // 
+            this.Btn8.Location = new System.Drawing.Point(312, 181);
+            this.Btn8.Name = "Btn8";
+            this.Btn8.Size = new System.Drawing.Size(73, 44);
+            this.Btn8.TabIndex = 5;
+            this.Btn8.UseVisualStyleBackColor = true;
+            this.Btn8.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // Btn7
+            // 
+            this.Btn7.Location = new System.Drawing.Point(233, 181);
+            this.Btn7.Name = "Btn7";
+            this.Btn7.Size = new System.Drawing.Size(73, 44);
+            this.Btn7.TabIndex = 6;
+            this.Btn7.UseVisualStyleBackColor = true;
+            this.Btn7.Click += new System.EventHandler(this.BtnAll_Click);
+            // 
+            // LblSira
+            // 
+            this.LblSira.AutoSize = true;
+            this.LblSira.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSira.Location = new System.Drawing.Point(538, 9);
+            this.LblSira.Name = "LblSira";
+            this.LblSira.Size = new System.Drawing.Size(162, 32);
+            this.LblSira.TabIndex = 7;
+            this.LblSira.Text = "Oyun Sirasi";
+            // 
+            // lblNovbe
+            // 
+            this.lblNovbe.AutoSize = true;
+            this.lblNovbe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovbe.Location = new System.Drawing.Point(594, 59);
+            this.lblNovbe.Name = "lblNovbe";
+            this.lblNovbe.Size = new System.Drawing.Size(34, 32);
+            this.lblNovbe.TabIndex = 7;
+            this.lblNovbe.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 32);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "O";
+            // 
+            // lblxscore
+            // 
+            this.lblxscore.AutoSize = true;
+            this.lblxscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblxscore.Location = new System.Drawing.Point(23, 91);
+            this.lblxscore.Name = "lblxscore";
+            this.lblxscore.Size = new System.Drawing.Size(31, 32);
+            this.lblxscore.TabIndex = 7;
+            this.lblxscore.Text = "0";
+            // 
+            // lbloscore
+            // 
+            this.lbloscore.AutoSize = true;
+            this.lbloscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbloscore.Location = new System.Drawing.Point(63, 91);
+            this.lbloscore.Name = "lbloscore";
+            this.lbloscore.Size = new System.Drawing.Size(31, 32);
+            this.lbloscore.TabIndex = 7;
+            this.lbloscore.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 32);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Hesab";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 254);
+            this.Controls.Add(this.lblNovbe);
+            this.Controls.Add(this.lbloscore);
+            this.Controls.Add(this.lblxscore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblSira);
+            this.Controls.Add(this.Btn9);
+            this.Controls.Add(this.Btn8);
+            this.Controls.Add(this.Btn7);
+            this.Controls.Add(this.Btn6);
+            this.Controls.Add(this.Btn5);
+            this.Controls.Add(this.Btn4);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Button Btn6;
+        private System.Windows.Forms.Button Btn5;
+        private System.Windows.Forms.Button Btn4;
+        private System.Windows.Forms.Button Btn9;
+        private System.Windows.Forms.Button Btn8;
+        private System.Windows.Forms.Button Btn7;
+        private System.Windows.Forms.Label LblSira;
+        private System.Windows.Forms.Label lblNovbe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblxscore;
+        private System.Windows.Forms.Label lbloscore;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Tick-Tack/Tick-Tack/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tick_Tack
+{
+    public partial class Form1 : Form
+    {
+        List<Button> Clicked = new List<Button>();
+        private string step = "X";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnAll_Click(object sender,EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text==string.Empty)
+            {
+                Clicked.Add(btn);
+                btn.Text = this.step;
+                if (this.step=="X")
+                {
+                    this.step = "O";
+                    lblNovbe.Text = this.step;
+                }
+                else
+                {
+                    this.step = "X";
+                    lblNovbe.Text = this.step;
+                }
+                Check();             
+            }            
+        }
+
+        private void Check()
+        {
+            if (Btn1.Text!=string.Empty && Btn1.Text==Btn2.Text && Btn2.Text==Btn3.Text)
+            {
+                MessageBox.Show(Btn1.Text+" "+"qalib oldu");
+                Score_Count();
+            }
+            if (Btn4.Text != string.Empty && Btn4.Text == Btn5.Text && Btn5.Text == Btn6.Text)
+            {
+                MessageBox.Show(Btn4.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn7.Text != string.Empty && Btn7.Text == Btn8.Text && Btn8.Text == Btn9.Text)
+            {
+                MessageBox.Show(Btn7.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn1.Text != string.Empty && Btn1.Text == Btn4.Text && Btn4.Text == Btn7.Text)
+            {
+                MessageBox.Show(Btn1.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn2.Text != string.Empty && Btn2.Text == Btn5.Text && Btn5.Text == Btn8.Text)
+            {
+                MessageBox.Show(Btn2.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn3.Text != string.Empty && Btn3.Text == Btn6.Text && Btn6.Text == Btn9.Text)
+            {
+                MessageBox.Show(Btn3.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn3.Text != string.Empty && Btn3.Text == Btn5.Text && Btn5.Text == Btn7.Text)
+            {
+                MessageBox.Show(Btn3.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            if (Btn1.Text != string.Empty && Btn1.Text == Btn5.Text && Btn5.Text == Btn9.Text)
+            {
+                MessageBox.Show(Btn1.Text + " " + "qalib oldu");
+                Score_Count();
+            }
+            else if (Btn1.Text != string.Empty && Btn2.Text != string.Empty && Btn3.Text != string.Empty && Btn4.Text != string.Empty && Btn5.Text != string.Empty && Btn6.Text != string.Empty && Btn7.Text != string.Empty && Btn8.Text != string.Empty && Btn9.Text != string.Empty)
+            {
+                MessageBox.Show("Beraberlik");
+                Reset();
+            }
+        }
+
+        private void Score_Count()
+        {
+            int xscore = Convert.ToInt32(lblxscore.Text);
+            int oscore = Convert.ToInt32(lbloscore.Text);
+            if (this.step == "O")
+                {
+                lblxscore.Text = (xscore + 1).ToString();
+                if (xscore==2)
+                {
+                    MessageBox.Show("Oyunun Qalibi X");
+                    lblxscore.Text = "0";
+                    lbloscore.Text = "0";
+                    this.step = "X";
+                    lblNovbe.Text = "X";
+                }
+                }
+              else
+               {
+                lbloscore.Text = (oscore + 1).ToString();
+                if (oscore == 2)
+                {
+                    MessageBox.Show("Oyunun Qalibi O");
+                    lblxscore.Text = "0";
+                    lbloscore.Text = "0";
+                    this.step = "O";
+                    lblNovbe.Text = "O";
+                }
+               }
+            Reset();          
+        }
+
+        private void Reset()
+        {
+            foreach (Button btn in Clicked)
+            {
+                btn.Text = string.Empty;
+            }
+            this.Clicked.Clear();
+        }
+    }
+}
